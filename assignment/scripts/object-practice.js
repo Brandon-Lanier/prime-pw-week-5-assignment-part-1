ee044987b0f244fdbd62dcd53e6d61ee05238995
Brandon Lanier <brandonjlanier@gmail.com>
2022-01-10 11:53:47 -0600

    Completed the necessary tasks

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,32 +10,44 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Brandon ',
+  lastName: 'Lanier',
+  hasSiblings: true,
+  shoeCount: 3,
+  favThreeFoods: ['Pangang Curry', 'Ramen', 'Tacos']
 };
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
+  - Create a variable called fullName
+  - Use the firstName & lastName properties of the object you
+    created above to set its value.
   - Console.log fullName
 */
+let fullName = me.firstName + me.lastName;
 
+console.log(fullName);
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
-  - Console.log your last favorite food 
+  - Console.log your last favorite food
 */
 
-
+console.log(`Logging first favorite food: ${me.favThreeFoods[0]}`);
+console.log(`Looging last favorite foodL: ${me.favThreeFoods[2]}`);
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
+  - Console.log your updated shoe count.
 */
+console.log(`Our current shoe count is ${me.shoeCount}`);
 
+me.shoeCount = 4; // This can also be done by using the += and setting it to 1.   me.shoeCount += 1;
+
+console.log(`We got new shoes, so now our shoe count is ${me.shoeCount}`);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +55,7 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'Blue';
+
+console.log(`Added a new property called favoriteColor, which is ${me.favoriteColor}`);
